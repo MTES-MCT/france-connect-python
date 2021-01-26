58f4a38b8bfe5db9e9638941f4579eee95c29b75
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2021-01-26 11:22:55 +0100

    Add URL endpoints

---
+++ project/franceconnect/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = "fc"
+
+urlpatterns = [
+    path("authorize", views.france_connect_authorize, name="authorize"),
+    path("callback", views.france_connect_callback, name="callback"),
+    path("logout", views.france_connect_logout, name="logout"),
+]
